be398c5e91689afd22af707233fe336ef18392d6
Konrad Banachowicz <konradb3@gmail.com>
2012-11-11 17:47:30 +0100

    ...

---
+++ robmod_plugin2/src/robmod_plugin2/NewLibraryProjectWizard.java
@@ -0,0 +1,297 @@
+package robmod_plugin2;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URI;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExecutableExtension;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.SubProgressMonitor;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.actions.WorkspaceModifyOperation;
+import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
+import org.eclipse.ui.wizards.newresource.BasicNewProjectResourceWizard;
+
+public class NewLibraryProjectWizard extends Wizard implements INewWizard,
+		IExecutableExtension {
+
+	/*
+	 * Use the WizardNewProjectCreationPage, which is provided by the Eclipse
+	 * framework.
+	 */
+	private WizardNewProjectCreationPage wizardPage;
+
+	private IConfigurationElement config;
+
+	private IWorkbench workbench;
+
+	private IStructuredSelection selection;
+
+	private IProject project;
+
+	/**
+	 * Constructor
+	 */
+	public NewLibraryProjectWizard() {
+		super();
+	}
+
+	public void addPages() {
+		wizardPage = new WizardNewProjectCreationPage(
+				"NewComponentLibraryProject");
+		wizardPage.setDescription("Create Components Library Project.");
+		wizardPage.setTitle("New Components Library Project");
+		addPage(wizardPage);
+	}
+
+	@Override
+	public boolean performFinish() {
+		if (project != null) {
+			return true;
+		}
+
+		final IProject projectHandle = wizardPage.getProjectHandle();
+
+		URI projectURI = (!wizardPage.useDefaults()) ? wizardPage
+				.getLocationURI() : null;
+
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+
+		final IProjectDescription desc = workspace
+				.newProjectDescription(projectHandle.getName());
+
+		desc.setLocationURI(projectURI);
+
+		/*
+		 * Just like the ExampleWizard, but this time with an operation object
+		 * that modifies workspaces.
+		 */
+		WorkspaceModifyOperation op = new WorkspaceModifyOperation() {
+			protected void execute(IProgressMonitor monitor)
+					throws CoreException {
+				createProject(desc, projectHandle, monitor);
+			}
+		};
+
+		/*
+		 * This isn't as robust as the code in the BasicNewProjectResourceWizard
+		 * class. Consider beefing this up to improve error handling.
+		 */
+		try {
+			getContainer().run(true, true, op);
+		} catch (InterruptedException e) {
+			return false;
+		} catch (InvocationTargetException e) {
+			Throwable realException = e.getTargetException();
+			MessageDialog.openError(getShell(), "Error",
+					realException.getMessage());
+			return false;
+		}
+
+		project = projectHandle;
+
+		if (project == null) {
+			return false;
+		}
+
+		BasicNewProjectResourceWizard.updatePerspective(config);
+		BasicNewProjectResourceWizard.selectAndReveal(project,
+				workbench.getActiveWorkbenchWindow());
+
+		return true;
+	}
+
+	/**
+	 * This creates the project in the workspace.
+	 * 
+	 * @param description
+	 * @param projectHandle
+	 * @param monitor
+	 * @throws CoreException
+	 * @throws OperationCanceledException
+	 */
+	void createProject(IProjectDescription description, IProject proj,
+			IProgressMonitor monitor) throws CoreException,
+			OperationCanceledException {
+		try {
+
+			monitor.beginTask("", 2000);
+
+			proj.create(description, new SubProgressMonitor(monitor, 1000));
+
+			if (monitor.isCanceled()) {
+				throw new OperationCanceledException();
+			}
+
+			proj.open(IResource.BACKGROUND_REFRESH, new SubProgressMonitor(
+					monitor, 1000));
+
+			/*
+			 * Okay, now we have the project and we can do more things with it
+			 * before updating the perspective.
+			 */
+			IContainer container = (IContainer) proj;
+
+			/* Add the style folder and the site.css file to it */
+			final IFolder modelFolder = container.getFolder(new Path("model"));
+			modelFolder.create(true, true, monitor);
+
+			InputStream resourceStream = createLibraryStream(proj.getName());
+
+			addFileToProject(container, new Path(modelFolder.getName()
+					+ Path.SEPARATOR + proj.getName() + ".robmod"), resourceStream, monitor);
+
+			resourceStream.close();
+
+			resourceStream = createLibraryDiagramStream(proj.getName());
+
+			addFileToProject(container, new Path(modelFolder.getName()
+					+ Path.SEPARATOR + proj.getName() + ".robmod_diagram"), resourceStream, monitor);
+
+			resourceStream.close();
+			
+			IFolder srcFolder = container.getFolder(new Path("src"));
+			srcFolder.create(true, true, monitor);
+		} catch (IOException ioe) {
+			IStatus status = new Status(IStatus.ERROR, "ExampleWizard",
+					IStatus.OK, ioe.getLocalizedMessage(), null);
+			throw new CoreException(status);
+		} finally {
+			monitor.done();
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.IWorkbenchWizard#init(org.eclipse.ui.IWorkbench,
+	 * org.eclipse.jface.viewers.IStructuredSelection)
+	 */
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		// snipped...
+	}
+
+	/**
+	 * Sets the initialization data for the wizard.
+	 */
+	public void setInitializationData(IConfigurationElement config,
+			String propertyName, Object data) throws CoreException {
+		// snipped...
+	}
+
+	/**
+	 * Adds a new file to the project.
+	 * 
+	 * @param container
+	 * @param path
+	 * @param contentStream
+	 * @param monitor
+	 * @throws CoreException
+	 */
+	private void addFileToProject(IContainer container, Path path,
+			InputStream contentStream, IProgressMonitor monitor)
+			throws CoreException {
+		final IFile file = container.getFile(path);
+		if (file.exists()) {
+			file.setContents(contentStream, true, true, monitor);
+		} else {
+			file.create(contentStream, true, monitor);
+		}
+	}
+	
+	/**
+	 * Initialize the file contents to contents of the given resource.
+	 */
+	public InputStream createLibraryStream(String title) throws CoreException {
+
+		/* We want to be truly OS-agnostic */
+		final String newline = System.getProperty("line.separator");
+
+		String line;
+		StringBuffer sb = new StringBuffer();
+
+		try {
+			InputStream input = this.getClass().getResourceAsStream(
+					"templates/library-robmod-template.resource");
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					input));
+			try {
+
+				while ((line = reader.readLine()) != null) {
+					line = line.replaceAll("\\$\\{name\\}", title);
+					sb.append(line);
+					sb.append(newline);
+				}
+
+			} finally {
+				reader.close();
+			}
+
+		} catch (IOException ioe) {
+			IStatus status = new Status(IStatus.ERROR, "NewFileWizard", IStatus.OK,
+					ioe.getLocalizedMessage(), null);
+			throw new CoreException(status);
+		}
+
+		return new ByteArrayInputStream(sb.toString().getBytes());
+	}
+	
+	/**
+	 * Initialize the file contents to contents of the given resource.
+	 */
+	public InputStream createLibraryDiagramStream(String title) throws CoreException {
+
+		/* We want to be truly OS-agnostic */
+		final String newline = System.getProperty("line.separator");
+
+		String line;
+		StringBuffer sb = new StringBuffer();
+
+		try {
+			InputStream input = this.getClass().getResourceAsStream(
+					"templates/library-robmod_diagram-template.resource");
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					input));
+			try {
+
+				while ((line = reader.readLine()) != null) {
+					line = line.replaceAll("\\$\\{name\\}", title);
+					sb.append(line);
+					sb.append(newline);
+				}
+
+			} finally {
+				reader.close();
+			}
+
+		} catch (IOException ioe) {
+			IStatus status = new Status(IStatus.ERROR, "NewFileWizard", IStatus.OK,
+					ioe.getLocalizedMessage(), null);
+			throw new CoreException(status);
+		}
+
+		return new ByteArrayInputStream(sb.toString().getBytes());
+	}
+}
